73aa95178aa5a3d2e69b90e446c997611dd09e45
sanchit-9068 <sanchitawasthi2525@gmail.com>
2024-03-09 14:52:41 +0530

    first commit

---
+++ image_dma.sdk/dma_contest/Debug/src/helloworld.d
@@ -0,0 +1,61 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../dma_contest_bsp/ps7_cortexa9_0/include/xil_cache.h
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../dma_contest_bsp/ps7_cortexa9_0/include/xil_cache.h:
---
+++ image_dma.srcs/sources_1/bd/image_test/ip/image_test_inverter_0_0/image_test_inverter_0_0_stub.v
@@ -0,0 +1,28 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
+// Date        : Fri Mar  8 15:00:10 2024
+// Host        : Sanchitslaptop running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               d:/Kishore_Sir_Project/image_dma/image_dma.srcs/sources_1/bd/image_test/ip/image_test_inverter_0_0/image_test_inverter_0_0_stub.v
+// Design      : image_test_inverter_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "inverter,Vivado 2018.2" *)
+module image_test_inverter_0_0(axi_clk, axi_reset_n, s_axis_valid, 
+  s_axis_data, s_axis_ready, m_axis_valid, m_axis_data, m_axis_ready)
+/* synthesis syn_black_box black_box_pad_pin="axi_clk,axi_reset_n,s_axis_valid,s_axis_data[31:0],s_axis_ready,m_axis_valid,m_axis_data[31:0],m_axis_ready" */;
+  input axi_clk;
+  input axi_reset_n;
+  input s_axis_valid;
+  input [31:0]s_axis_data;
+  output s_axis_ready;
+  output m_axis_valid;
+  output [31:0]m_axis_data;
+  input m_axis_ready;
+endmodule
